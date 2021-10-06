67345205994be3d5f8cd009d3fb52ab2d5b8d02d
difeorqui <dfoq76@hotmail.com>
2021-10-05 22:53:35 -0500

    se carga la carpeta de las api para el front

---
+++ PAWEB_FRONT/src/api/index.js
@@ -0,0 +1,11 @@
+import auth from "./auth";
+import user from "./user";
+import person from "./person";
+import machine from "./machine";
+
+export default {
+  auth,
+  user,
+  person,
+  machine
+};
